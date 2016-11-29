007685a42f96fa6119cda87cf36a96cf616fc59d
bcoppingHOF <bcopping@hof.co.uk>
2016-11-29 15:38:16 +0000

    method to show initial variant and update variant

--- src/js/components/colourSwatches.js
+++ src/js/components/colourSwatches.js
@@ -2,11 +2,30 @@ import Vue from 'vue';
 //color swatch list component
 
 export default Vue.component('colour-swatch', {
-  props: ['colours'],
+  props: ['colours', 'selected'],
+ 
   template: `
     <div class="colourSwatchWrapper">
         <div v-for="colour in colours" 
         v-bind:class="colour.availability" 
-        v-text="colour.colour"></div>
+        v-text="colour.colour"
+        v-on:click="onSwatchClick(colour.colour)"></div>
+        <div>Selected Color: <span>{{selectedColour}}</span></div>
     </div>`,
+    
+    methods: {
+        onSwatchClick: function(payload) {
+            console.log(payload)
+            //this.selected = i
+            //console.log(this.selected)
+            this.$emit('action','CHANGE_VARIANT', payload);
+            
+        }
+    },
+    computed: {
+        selectedColour: function(){
+            console.log('this selected is', this.selected)
+            return this.selected;
+        }
+    }
 });
