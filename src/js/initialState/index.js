c2e07d6709496a84e21ac9cfcebce421da026689
bcoppingHOF <bcopping@hof.co.uk>
2016-11-29 15:34:53 +0000

    update the initial state

--- src/js/initialState/index.js
+++ src/js/initialState/index.js
@@ -1,4 +1,12 @@
 export default ({
     name: 'first name',
-    colours: []
+    colours: [
+        { colour: 'red', availability: 'available' },
+        { colour: 'teal', availability:   'available'},
+        { colour: 'blue', availability:  'available' },
+        { colour: 'yellow', availability:  'available' },
+        { colour: 'green', availability:  'available' },
+        { colour: 'black', availability:  'available' }
+    ],
+    selectedColour: 'red'
 })
